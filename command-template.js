c9cdea6b890b466d485d43da97e4d85516992e2f
Vortex864 <78692711+Vortex864@users.noreply.github.com>
2021-04-16 21:41:57 +0400

    Create command-template.js

---
+++ command-template.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "COMMAND NAME", // Put your command name. This is necessary.
+  aliases: [], // If you want to put any aliases, you can add them in this array. It is optional whether you want it or not. Example: aliases: ['k']
+  usage: "HOW TO USE THE COMMAND", // This describes on how do we use this command. An example of this is: -kick <member> <reason>
+  description: "WHAT THE COMMAND DOES", // This describes on what the command actually does. For example: "This command kicks a member."
+  run: async (client, message, args) => {
+  
+  // And finally, this is where you input in your code. So.. that's it!
+  
+  
+  }
+}
